04ae5a376c25a10e02b555c3860a9a47f853ed94
ivanstepanov <ivanstepanoff@yahoo.com>
2022-05-08 22:06:20 +0200

    feat: detect OS,highlight pressed button with animation, input text by real keyboard

---
+++ js/windows-layout.js
@@ -0,0 +1,110 @@
+export const win = `<h1>Windows ENG keyboard</h1>
+
+            <div class="keyboard-row">
+                <div class="key" data-code="Backquote"><span>~<br/>\`</span></div>
+                <div class="key" data-code="Digit1"><span>!<br/>1</span></div>
+                <div class="key" data-code="Digit2"><span>@<br/>2</span></div>
+                <div class="key" data-code="Digit3"><span>#<br/>3</span></div>
+                <div class="key" data-code="Digit4"><span>$<br/>4</span></div>
+                <div class="key" data-code="Digit5"><span>%<br/>5</span></div>
+                <div class="key" data-code="Digit6"><span>^<br/>6</span></div>
+                <div class="key" data-code="Digit7"><span>&amp;<br/>7</span></div>
+                <div class="key" data-code="Digit8"><span>*<br/>8</span></div>
+                <div class="key" data-code="Digit9"><span>(<br/>9</span></div>
+                <div class="key" data-code="Digit0"><span>)<br/>0</span></div>
+                <div class="key" data-code="Minus"><span>_<br/>-</span></div>
+                <div class="key" data-code="Equal"><span>+<br/>=</span></div>
+                <div class="key  extra-size lowercase lower-right" data-code="Delete">
+                    <span class="lower-row-text">delete</span>
+                </div>
+            </div>
+
+            <div class="keyboard-row">
+                <div class="key extra-size lowercase lower-left" data-code="Tab">
+                    <span class="lower-row-text">tab</span>
+                </div>
+                <div class="key" data-code="KeyQ">Q</div>
+                <div class="key" data-code="KeyW">W</div>
+                <div class="key" data-code="KeyE">E</div>
+                <div class="key" data-code="KeyR">R</div>
+                <div class="key" data-code="KeyT">T</div>
+                <div class="key" data-code="KeyY">Y</div>
+                <div class="key" data-code="KeyU">U</div>
+                <div class="key" data-code="KeyI">I</div>
+                <div class="key" data-code="KeyO">O</div>
+                <div class="key" data-code="KeyP">P</div>
+                <div class="key" data-code="BracketLeft"><span>{<br/>[</span></div>
+                <div class="key" data-code="BracketRight"><span>}<br/>]</span></div>
+                <div class="key" data-code="Backslash"><span>|<br/>\\</span></div>
+            </div>
+
+            <div class="keyboard-row">
+                <div class="key lowercase lower-left extra-size-two" data-code="CapsLock">
+                    <span class="lower-row-text">caps lock</span>
+                    <span class="absolute-left caps-dot">&bull;</span>
+                </div>
+                <div class="key" data-code="KeyA">A</div>
+                <div class="key" data-code="KeyS">S</div>
+                <div class="key" data-code="KeyD">D</div>
+                <div class="key" data-code="KeyF">F</div>
+                <div class="key" data-code="KeyG">G</div>
+                <div class="key" data-code="KeyH">H</div>
+                <div class="key" data-code="KeyJ">J</div>
+                <div class="key" data-code="KeyK">K</div>
+                <div class="key" data-code="KeyL">L</div>
+                <div class="key" data-code="Semicolon"><span>:<br/>;</span></div>
+                <div class="key" data-code="Quote"><span>"<br/>'</span></div>
+                <div class="key extra-size-two lowercase lower-right"  data-code="Enter">
+                    <span class="lower-row-text">enter</span>
+                </div>
+            </div>
+
+            <div class="keyboard-row">
+                <div class="key double-size lowercase lower-left" data-code="ShiftLeft">
+                    <span class="lower-row-text">shift</span>
+                </div>
+                <div class="key" data-code="KeyZ">Z</div>
+                <div class="key" data-code="KeyX">X</div>
+                <div class="key" data-code="KeyC">C</div>
+                <div class="key" data-code="KeyV">V</div>
+                <div class="key" data-code="KeyB">B</div>
+                <div class="key" data-code="KeyN">N</div>
+                <div class="key" data-code="KeyM">M</div>
+                <div class="key" data-code="Comma"><span>&lt;<br/>,</span></div>
+                <div class="key" data-code="Period"><span>&gt;<br/>.</span></div>
+                <div class="key" data-code="Slash"><span>?<br/>/</span></div>
+                <div class="key double-size lowercase lower-right"  data-code="ShiftRight">
+                    <span class="lower-row-text">shift</span>
+                </div>
+            </div>
+
+            <div class="keyboard-row bottom-row">
+                <div class="key lower-left lowercase"  data-code="ControlLeft">
+                    <span class="lower-row-text">Ctrl</span>
+                </div>
+                <div class="key lower-left lowercase" data-code="MetaLeft">
+                    <span class="lower-row-text">Win</span>
+                </div>
+                <div class="key lower-left lowercase" data-code="AltLeft">
+                    <span class="lower-row-text">Alt</span>
+                </div>
+                <div class="key lower-center lowercase extra-size-two" data-code="IntlBackslash">
+                    <span class="lower-row-text">\\</span>
+                    <span class="absolute-right icon">|</span>
+                </div>
+                <div class="key space-bar" data-code="Space"></div>
+                <div class="key lower-center lowercase extra-size-two" data-code="AltRight">
+                    <span class="lower-row-text">Alt</span>
+                </div>
+                <div class="key lower-right lowercase" data-code="ControlRight">
+                    <span class="lower-row-text">Ctrl</span>
+                </div>
+                <div class="arrows">
+                    <div class="key" data-code="ArrowLeft">&#9668;</div>
+                    <div class="up-down">
+                        <div class="key" data-code="ArrowUp">&#9650;</div>
+                        <div class="key" data-code="ArrowDown">&#9660;</div>
+                    </div>
+                    <div class="key" data-code="ArrowRight">&#9658;</div>
+                </div>
+            </div>`
---
+++ main.js
@@ -0,0 +1,87 @@
+import {macos} from './js/macos-layout.js'
+import {win} from './js/windows-layout.js'
+
+// shiftKey Win === OS X
+// altKey Alt Win === Option OS X
+// ctrlKey Ctrl Win === Command OS X
+// on Windows 'metaKey' does not work? - keyCode, charCode, which instead;
+// Control btn OS X sets the property in the keypress and keydown events, but not in the keyup
+
+let lang = localStorage.getItem('keyboard-lang') ? localStorage.getItem('keyboard-lang') : 'eng'
+const os = navigator.appVersion.indexOf('Mac') !== -1
+    ? 'MacOS'
+    : navigator.appVersion.indexOf('Win') !== -1 ? 'Windows' : 'Unknown OS'
+
+const keyboard = os === 'MacOS'
+    ? macos
+    : os === 'Windows'
+    ? win
+    : "You've got it, dude..."
+let text
+
+window.onload = function() {
+    document
+        .querySelector('.app-container')
+        .innerHTML = `<div class="text-container">
+            <textarea class="text" readonly cols="160" rows="15" placeholder="Enter your wish..."></textarea>
+        </div>
+        <div class="keyboard-container">
+            ${keyboard}
+        </div>`
+    text = document.querySelector('.text')
+
+    window.addEventListener('keydown', keyboardHandler)
+    document
+        .querySelector('.keyboard-container')
+        .addEventListener('mousedown', btnPressHandler)
+}
+
+const keyboardHandler = e => {
+    const btn = document.querySelector(`[data-code="${e.code}"]`)
+    const cancelHighlite = e => {
+        if (e.code === btn.dataset.code) {
+            btn.style.backgroundColor = '#f7f7f7'
+            document.removeEventListener('keyup', cancelHighlite)
+        }
+    }
+    document.addEventListener('keyup', cancelHighlite)
+    btn.style.backgroundColor = 'rgba(177, 232, 244, 0.4)'
+    if (e.key !== undefined) {
+        if (e.key === 'Tab') {
+            e.preventDefault()
+            text.innerHTML  += '    '
+        } else if (e.key === 'Alt') {
+            e.preventDefault()
+        } else if (e.key === ' ') {
+            e.preventDefault()
+            text.textContent += ' '
+        } else if (e.key === 'ArrowUp') {
+            text.innerHTML  += '&#9650;'
+        } else if (e.key === 'ArrowDown') {
+            text.innerHTML  += '&#9660;'
+        } else if (e.key === 'ArrowLeft') {
+            text.innerHTML  += '&#9668;'
+        } else if (e.key === 'ArrowRight') {
+            text.innerHTML += '&#9658;'
+        } else if (e.key === 'Enter') {
+             text.innerHTML += '\n'
+        } else if (e.key !== 'Shift'
+            && e.key !== 'Control'
+            && e.key !== 'CapsLock'
+            && e.key !== 'Delete'
+            && e.key !== 'Backspace'
+            && e.key !== 'Insert'
+            && e.key !== 'PageUp'
+            && e.key !== 'PageDown'
+            && e.key !== 'Home'
+            && e.key !== 'End'
+            && e.key !== 'ContextMenu'
+            && e.key !== 'Meta')  text.textContent += e.key
+    }
+}
+
+const btnPressHandler = e => {
+    if (e.target.classList.contains('key') || e.target.parentElement.classList.contains('key')) {
+        text.textContent += e.target.dataset.code
+    }
+}
