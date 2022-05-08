5dfd51a142e2fb26919b06392a03974b67e2969d
ivanstepanov <ivanstepanoff@yahoo.com>
2022-05-08 18:07:17 +0200

    feat: create keyboard layout

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:airbnb-base",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
